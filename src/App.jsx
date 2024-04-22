9e9872dff4b2e9a6ee349a7fc0c23cdf88c193b6
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-22 06:57:49 +0200

    Render CV components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
+import GeneralInformationView from "./components/view/GeneralInformationView";
+import EducationView from "./components/view/EducationView";
+import ExperienceView from "./components/view/ExperienceView";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <GeneralInformationView
+        name="John"
+        email="john@doe.com"
+        number="123123123"
+      />
+      <h2 className="text-lg font-bold text-center m-4">EDUCATION</h2>
+      <EducationView
+        schoolName="Sample School"
+        titleOfStudy="Computer Science"
+        dateOfStudy="2020/03/01 - 2023/03/01"
+      />
+      <h2 className="text-lg font-bold text-center m-4">EXPERIENCE</h2>
+      <ExperienceView
+        companyName="The Company"
+        positionTitle="Front End Developer"
+        dateOfWork="2023/03/01 - current"
+        mainResponsibilities="This is a place for main responsibilities."
+      />
+    </div>
   );
 }
 
