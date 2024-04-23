0d42bfd5d5f5f128743ea7000b4a484e26b78a80
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-23 07:34:34 +0200

    Move all components into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,50 @@
-import GeneralInformationView from "./components/view/GeneralInformationView";
-import EducationView from "./components/view/EducationView";
-import ExperienceView from "./components/view/ExperienceView";
-import GeneralInformationForm from "./components/form/GeneralInformationForm";
+import { useState } from "react";
 function App() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="flex flex-col h-screen">
-        <GeneralInformationForm />
+        <div className="min-h-24 p-4 m-2 shadow-md rounded w-full">
+          <form action="/" className="flex flex-col gap-2">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="bg-slate-100 rounded"
+            />
+            <label htmlFor="email">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="bg-slate-100 rounded"
+            />
+            <label htmlFor="number">Phone Number</label>
+            <input
+              type="tel"
+              name="number"
+              id="number"
+              className="bg-slate-100 rounded"
+            />
+          </form>
+        </div>
       </div>
       <div className="flex flex-col h-screen">
-        <GeneralInformationView
-          name="John"
-          email="john@doe.com"
-          number="123123123"
-        />
+        <div className="min-h-24 p-4 m-2 shadow-md rounded w-full">
+          <h2 className="text-lg text-center"></h2>
+          <p className="text-md text-center">|</p>
+        </div>
         <h2 className="text-lg font-bold text-center m-4">EDUCATION</h2>
-        <EducationView
-          schoolName="Sample School"
-          titleOfStudy="Computer Science"
-          dateOfStudy="2020/03/01 - 2023/03/01"
-        />
+        <div className="min-h-24 p-4 m-2 shadow-md rounded w-full">
+          <h2 className="text-lg text-center"></h2>
+          <p className="text-md text-center">|</p>
+        </div>
         <h2 className="text-lg font-bold text-center m-4">EXPERIENCE</h2>
-        <ExperienceView
-          companyName="The Company"
-          positionTitle="Front End Developer"
-          dateOfWork="2023/03/01 - current"
-          mainResponsibilities="This is a place for main responsibilities."
-        />
+        <div className="min-h-24 p-4 m-2 shadow-md rounded w-full">
+          <h2 className="text-lg text-center"></h2>
+          <p className="text-md text-center">|</p>
+          <p className="text-center mt-4"></p>
+        </div>
       </div>
     </div>
   );
