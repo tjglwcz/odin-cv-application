b472a9f64f4f3352079013fd29af29689ebdc9ab
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-23 07:03:46 +0200

    Changhe the layout in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
 import GeneralInformationView from "./components/view/GeneralInformationView";
 import EducationView from "./components/view/EducationView";
 import ExperienceView from "./components/view/ExperienceView";
+import GeneralInformationForm from "./components/form/GeneralInformationForm";
 function App() {
   return (
-    <div>
-      <GeneralInformationView
-        name="John"
-        email="john@doe.com"
-        number="123123123"
-      />
-      <h2 className="text-lg font-bold text-center m-4">EDUCATION</h2>
-      <EducationView
-        schoolName="Sample School"
-        titleOfStudy="Computer Science"
-        dateOfStudy="2020/03/01 - 2023/03/01"
-      />
-      <h2 className="text-lg font-bold text-center m-4">EXPERIENCE</h2>
-      <ExperienceView
-        companyName="The Company"
-        positionTitle="Front End Developer"
-        dateOfWork="2023/03/01 - current"
-        mainResponsibilities="This is a place for main responsibilities."
-      />
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="flex flex-col h-screen">
+        <GeneralInformationForm />
+      </div>
+      <div className="flex flex-col h-screen">
+        <GeneralInformationView
+          name="John"
+          email="john@doe.com"
+          number="123123123"
+        />
+        <h2 className="text-lg font-bold text-center m-4">EDUCATION</h2>
+        <EducationView
+          schoolName="Sample School"
+          titleOfStudy="Computer Science"
+          dateOfStudy="2020/03/01 - 2023/03/01"
+        />
+        <h2 className="text-lg font-bold text-center m-4">EXPERIENCE</h2>
+        <ExperienceView
+          companyName="The Company"
+          positionTitle="Front End Developer"
+          dateOfWork="2023/03/01 - current"
+          mainResponsibilities="This is a place for main responsibilities."
+        />
+      </div>
     </div>
   );
 }
