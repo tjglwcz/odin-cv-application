0b7c363902388e3c61d94676d15a202bd2bbb129
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-23 07:42:26 +0200

    Make the general section update on form change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import { useState } from "react";
 function App() {
+  const [general, setGeneral] = useState({
+    name: "John Doe",
+    email: "john@doe.com",
+    number: "123321123",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = event.target;
+    setGeneral({
+      ...general,
+      [name]: value,
+    });
+  };
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="flex flex-col h-screen">
@@ -10,6 +24,8 @@ function App() {
               type="text"
               name="name"
               id="name"
+              value={general.name}
+              onChange={handleInputChange}
               className="bg-slate-100 rounded"
             />
             <label htmlFor="email">E-mail</label>
@@ -17,6 +33,8 @@ function App() {
               type="email"
               name="email"
               id="email"
+              value={general.email}
+              onChange={handleInputChange}
               className="bg-slate-100 rounded"
             />
             <label htmlFor="number">Phone Number</label>
@@ -24,6 +42,8 @@ function App() {
               type="tel"
               name="number"
               id="number"
+              value={general.number}
+              onChange={handleInputChange}
               className="bg-slate-100 rounded"
             />
           </form>
@@ -31,8 +51,10 @@ function App() {
       </div>
       <div className="flex flex-col h-screen">
         <div className="min-h-24 p-4 m-2 shadow-md rounded w-full">
-          <h2 className="text-lg text-center"></h2>
-          <p className="text-md text-center">|</p>
+          <h2 className="text-lg text-center">{general.name}</h2>
+          <p className="text-md text-center">
+            {general.email} | {general.number}
+          </p>
         </div>
         <h2 className="text-lg font-bold text-center m-4">EDUCATION</h2>
         <div className="min-h-24 p-4 m-2 shadow-md rounded w-full">
